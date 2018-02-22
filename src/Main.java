784770f107b7cf884805121161df96c59db4b829
5ia15cyneuschwander <5ia15cyneuschwander@gibwintra.ch>
2018-02-22 12:15:59 +0100

    added helloWorld

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,9 @@
+import lejos.hardware.Button;
+import lejos.hardware.lcd.LCD;
+
 public class Main {
+    public static void main(String[] args) {
+        LCD.drawString("helloWorld", 0, 0);
+        Button.waitForAnyPress();
+    }
 }
